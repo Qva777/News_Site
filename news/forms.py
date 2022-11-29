fb0e6498ecba5bbcaf8b9f46a72a7e82717dd540
Qva777 <barshay.sasha@gmail.com>
2022-11-29 23:53:59 +0200

    Added bootstrap style, new templates

---
+++ news/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from news.models import Category
+
+attrs = {'class': 'form-control'}
+
+
+class NewsForm(forms.Form):
+    title = forms.CharField(label="Наименование", max_length=150, widget=forms.TextInput(attrs=attrs))
+    content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'rows': 5
+    }))
+    is_published = forms.BooleanField(label='Опубликовно', initial=True, widget=forms.TextInput(attrs=attrs))
+    category = forms.ModelChoiceField(label='Категория', empty_label="Chose the category",
+                                      queryset=Category.objects.all(), widget=forms.Select(attrs=attrs))
