0c33437e7636c2285da87601d0550c9c512b41a5
Qva777 <barshay.sasha@gmail.com>
2022-12-03 17:41:49 +0200

    Delete comment

--- news/views.py
+++ news/views.py
@@ -48,35 +48,3 @@ class CreateNews(CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
     # success_url = reverse_lazy('home')
-
-# Preparing to delete
-# def index(request):
-#     news = News.objects.all()
-#     context = {
-#         'news': news,
-#         'title': 'Список новостей',
-#     }
-#     return render(request, template_name='news/index.html', context=context)
-
-#
-# def get_category(request, category_id):
-#     news = News.objects.filter(category_id=category_id)
-#     category = Category.objects.get(pk=category_id)
-#     return render(request, 'news/category.html', {'news': news, 'category': category})
-#
-#
-# def view_news(request, news_id):
-#     # news_item = News.objects.get(pk=news_id)
-#     news_item = get_object_or_404(News, pk=news_id)
-#     return render(request, 'news/view_news.html', {'news_item': news_item})
-
-
-# def add_news(request):
-#     if request == 'POST':
-#         form = NewsForm(request.POST)
-#         if form.is_valid():
-#             news = form.save()
-#             return redirect(news)
-#     else:
-#         form = NewsForm()
-#     return render(request, 'news/add_news.html', {'form': form})
