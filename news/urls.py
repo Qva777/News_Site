cace7dd37540e43f9cd5af114620db6fadc5d0f2
Qva777 <barshay.sasha@gmail.com>
2022-12-05 23:58:04 +0200

    Preparing to add pagination

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     # path('', index, name='home'),
+    path('test/', test, name='test'),
     path('', HomeNews.as_view(), name='home'),
 
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
