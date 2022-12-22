882c0046893a04447e3de4ea52ff4cf3fb84d562
qvashquai <barshay.sasha@gmail.com>
2022-12-22 14:42:56 +0200

    Update shell files for start

--- first_start.bat
+++ first_start.bat
@@ -1,5 +1,7 @@
 python -m venv venv
 .\venv\Scripts\activate
 pip install -r requirements.txt
+echo SECRET_KEY=YOUR_SECRET_KEY > .env
+echo DEBUG=True >> .env
 python manage.py migrate
 python manage.py runserver
--- first_start.sh
+++ first_start.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+echo SECRET_KEY=YOUR_SECRET_KEY > .env
+echo DEBUG=True >> .env
 python3 manage.py migrate
 python3 manage.py runserver
